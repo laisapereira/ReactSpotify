6ba0f5f6e8223d55e811fdcde12c69396584bdc5
Laisa Pereira <laisasilva.pereira@yahoo.com>
2023-04-06 02:53:14 -0300

    puxando tracks de cada album, ainda com bugs

--- src/Router/routes.tsx
+++ src/Router/routes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
 import ListTrack from "../pages/ListTrack";
 import Login from "../pages/Login";
@@ -7,7 +7,6 @@ import { setClientToken } from "../api/spotify";
 
 export function Router() {
   const [token, setToken] = useState("");
- 
 
   useEffect(() => {
     const token = window.localStorage.getItem("token");
@@ -18,24 +17,24 @@ export function Router() {
       const _token = hash.split("&")[0].split("=")[1];
       window.localStorage.setItem("token", _token);
       setToken(_token);
-      setClientToken(_token)
+      setClientToken(_token);
     } else {
       setToken(token as string);
-      setClientToken(token as string)
+      setClientToken(token as string);
     }
   }, []);
 
-
   return !token ? (
-    <BrowserRouter>
-      <Login />
-    </BrowserRouter>
+    <Routes>
+      <Route element={<Login />} path="/" />
+    </Routes>
   ) : (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Home />} path="/" />
-        <Route element={<ListTrack />} path="/list-track" />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route element={<Home />} path="/" />
+      <Route
+        element={<ListTrack/> }
+        path="/albums/:id/tracks" 
+      />
+    </Routes>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,15 @@
 import { useState, useCallback } from "react";
-import { IAlbumSpotify } from "../../Types/types";
+import { IAlbumSpotify } from "../../Types/albums";
 import { HomeContainer } from "./styles";
 import { apiClient } from "../../api/spotify";
 import { debounce } from "../../Utils/debounce";
+import { Link } from "react-router-dom";
 
 export function Home() {
   const [albums, setAlbums] = useState<IAlbumSpotify | null>();
-
+  
   const token = window.localStorage.getItem("token");
-
+  
   const searchAlbum = async (searchKey: string) => {
     if (searchKey === "") return setAlbums(null);
 
@@ -23,7 +24,6 @@ export function Home() {
           limit: 10,
         },
       });
-
       setAlbums(albumResponse.data);
       console.log(albums?.albums);
     } catch (error) {
@@ -35,6 +35,11 @@ export function Home() {
     debounce((searchKey: string) => searchAlbum(searchKey), 100),
     []
   );
+
+ /*  const navigate = useNavigate();
+ /*  const handleAlbumClick = (albumId:string) => {
+    navigate(`/albums/${albumId}/tracks`)
+  } */ 
 
   return (
     <HomeContainer>
@@ -56,6 +61,8 @@ export function Home() {
             <img src={album.images[0].url} alt="" />
             <h2>{album.name}</h2>
             <h3>{album.artists[0].name}</h3>
+            <h3>{album.release_date.split("")}</h3>
+            <Link to={`/albums/${album.id}/tracks`}><button>Ver faixas</button></Link>
           </div>
         ))}
       </div>
